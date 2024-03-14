da1943cfe377e56ca54072b995b41baff8d7c35d
VL.dilankarw <dilankarashmika10@gmail.com>
2024-03-14 22:38:54 +0530

    Employee Controller

--- springboot-backend/src/main/java/com/example/springbootbackend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -1,24 +1,28 @@
 package com.example.springbootbackend.controller;
 
-
 import com.example.springbootbackend.model.Employee;
 import com.example.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    //get all employee
-    @GetMapping("/employee")
-    public List<Employee> getAllEmployee(){
+    @PostMapping("/employee")
+    Employee newEmployee(@RequestBody Employee newEmployee){
+        return employeeRepository.save(newEmployee);
+    }
+
+    @GetMapping("/employees")
+    List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
     }
 }
