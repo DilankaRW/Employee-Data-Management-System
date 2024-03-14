d96b20b9abdc97a1166cc2f6668bcf3d888effc2
VL.dilankarw <dilankarashmika10@gmail.com>
2024-03-15 00:03:25 +0530

    Update Navbar

--- react-frontend/src/layout/Navbar.jsx
+++ react-frontend/src/layout/Navbar.jsx
@@ -7,11 +7,11 @@ export default function Navbar() {
 
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Employee Data</a>
+                <a className="navbar-brand" href="#">Employee Data Management System</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
-                <button className='btn btn-outline-light'>Add Employee</button>
+                <button className='btn btn-light'>Add Employee</button>
             </div>
         </nav>
 
