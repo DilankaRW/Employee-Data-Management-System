e490efa595fc9888e2c967d6450efd6ea4583125
VL.dilankarw <dilankarashmika10@gmail.com>
2024-03-14 23:57:36 +0530

    Add Navbar

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './layout/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+      <Navbar/>
     </div>
   );
 }
