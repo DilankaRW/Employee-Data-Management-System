8ae22d7c8ff2c5f45219ddceff28e05bae39d4e6
VL.dilankarw <dilankarashmika10@gmail.com>
2024-03-15 23:55:42 +0530

    Update

--- react-frontend/src/layout/Navbar.jsx
+++ react-frontend/src/layout/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
@@ -11,7 +12,7 @@ export default function Navbar() {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
-                <button className='btn btn-light'>Add Employee</button>
+                <Link className='btn btn-light' to="/addemployee">Add Employee</Link>
             </div>
         </nav>
 
