2a8fb8ba0689596bc6f58ff66486678bee4acf9e
VL.dilankarw <dilankarashmika10@gmail.com>
2024-03-16 15:22:03 +0530

    Update React Routing

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,18 +2,21 @@ import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './layout/Navbar';
 import Home from './Pages/Home';
-import { Route, Router, BrowserRouter as router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddEmployee from './employees/AddEmployee';
 
 function App() {
   return (
     <div className="App">
+
       <Router>
       <Navbar/>
       <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/addemployee' element={<AddEmployee/>}/>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/addemployee" element={<AddEmployee/>}/>
       </Routes>
       </Router>
+      
     </div>
   );
 }
