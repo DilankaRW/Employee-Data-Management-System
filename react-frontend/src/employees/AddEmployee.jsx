8275690fde92b8760fb97da52cdd406d263e8eeb
VL.dilankarw <dilankarashmika10@gmail.com>
2024-03-16 16:27:32 +0530

    AddEmployee Form Update

--- react-frontend/src/employees/AddEmployee.jsx
+++ react-frontend/src/employees/AddEmployee.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AddEmployee() {
+
+  const [employee, setEmployee] = useState({
+    firstname:"",
+    lastname:"",
+    email:""
+  })
+
+  const{firstname, lastname, email}=employee
+
+  const onIputChange=(e)=>{
+    setEmployee({...employee, [e.target.name]: e.target.value})
+  }
+
   return (
     <div className='container'>
       <div className='row'>
@@ -8,15 +21,15 @@ export default function AddEmployee() {
           <h2 className='text-center m-4'>Add New Employee</h2>
           <div className='mb-3'>
             <label htmlFor="Firstname" className='form-label'>First Name</label>
-            <input type='text' className='form-control' placeholder='Enter your First Name' name='firstname'/>
+            <input type='text' className='form-control' placeholder='Enter your First Name' name='firstname' value={firstname} onChange={(e)=>onIputChange(e)}/>
           </div>
           <div className='mb-3'>
             <label htmlFor="Lastname" className='form-label'>Last Name</label>
-            <input type='text' className='form-control' placeholder='Enter your Last Name' name='lastname'/>
+            <input type='text' className='form-control' placeholder='Enter your Last Name' name='lastname' value={lastname} onChange={(e)=>onIputChange(e)}/>
           </div>
           <div className='mb-3'>
             <label htmlFor="Email" className='form-label'>Email</label>
-            <input type='text' className='form-control' placeholder='Enter your Email' name='email'/>
+            <input type='text' className='form-control' placeholder='Enter your Email' name='email' value={email} onChange={(e)=>onIputChange(e)}/>
           </div>
           <button type='submit' className='btn btn-outline-primary'>Submit</button>
           <button type='submit' className='btn btn-outline-danger mx-2'>Cancel</button>
